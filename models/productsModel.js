6021302cad209b25a91a79a1d2c26d4844225b3d
Juan Marti <juanma.marti@gmail.com>
2022-10-30 21:23:32 -0300

    Finished products router

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,24 +1,28 @@
-import { fs } from 'fs';
+import fs from 'fs';
 
-class ProductModel {
+export default class ProductModel {
   constructor() {
-    this.file = "products.db.json"
+    this.file = "./models/products.db.json"
   }
 
   createProduct = async (product) => {
     try {
-      const fileContent = await fs.promises.readFile(this.file, 'utf-8').json()
-      const newProduct = {...product, id: fileContent.length + 1}
-      fileContent.push(newProduct)
-      await fs.promises.writeFile(this.file, JSON.stringify(fileContent, 'utf-8'))
+      const fileContent = await fs.promises.readFile(this.file, 'utf-8')
+      let contentParsed = JSON.parse(fileContent)
+      const fecha = new Intl.DateTimeFormat('es-AR').format(Date.now())
+      const newProduct = {...product, id: contentParsed.length + 1, timestamp: fecha}
+      contentParsed.push(newProduct)
+      console.log(contentParsed)
+      await fs.promises.writeFile(this.file, JSON.stringify(contentParsed, 'utf-8'))
+      return {response: 'success'}
     } catch (error) {
-      throw new Error(`Error al escribir el archivo: ${error}`)
+      return {'response': 'error: ' + error}
     }
   }
 
   getAllProducts = async () => {
     try {
-        const file = await fs.promises.readFile(this.name, 'utf-8')
+        const file = await fs.promises.readFile(this.file, 'utf-8')
         return JSON.parse(file)
     } catch (error) {
         throw new Error(`Error reading file ${error}`)
@@ -26,17 +30,29 @@ class ProductModel {
   }
 
   getOneProduct = async (id) => {
+    console.log(id)
     const products = await this.getAllProducts()
-    const filteredProduct = products.filter(e => e.id === Number)
+    console.log("Todos los productos: ", products)
+    const filteredProduct = products.filter(e => e.id === id)
+    console.log("Producto: ", filteredProduct)
     return filteredProduct
   }
 
   deleteProduct = async (id) => {
     const products = await this.getAllProducts()
-    const filteredProduct = products.filter(e => e.id != Number)
+    const filteredProduct = products.filter(e => e.id != id)
     await fs.promises.writeFile(this.file, JSON.stringify(filteredProduct),'utf-8')
-    return filteredProduct
+    return {response: "success"}
   }
 
-  updateProduct = async (product) => {}
+  updateProduct = async (id, product) => {
+    console.log(product)
+    const products = await this.getAllProducts()
+    const objIndex = products.findIndex((obj => obj.id == id))
+    products[objIndex] = {...product, id: Number(id)}
+
+    // console.log(products)
+    await fs.promises.writeFile(this.file, JSON.stringify(products),'utf-8')
+    return {response: "success"}
+  }
 }
--- routes/products/index.js
+++ routes/products/index.js
@@ -1,24 +1,43 @@
 import express from 'express'
+import productModel from '../../models/productsModel.js'
 
 const productRouter = express.Router()
 
-productRouter.get('/:id?', (req, res) => {
-  if (req.params.id){
-    return res.status(200).send('Un producto')
+let productmodel = new productModel()
+
+productRouter.post('/', async (req, res) => {
+  const data = req.body
+  console.log(data)
+  const response = await productmodel.createProduct(data)
+  console.log(response)
+  if (response.response === "success"){
+    return res.status(200).send(response)
+  } else {
+    return res.status(500).send(response)
   }
-  return res.status(200).send('Listado de productos')
 })
 
-productRouter.post('/', (req, res) => {
-  return res.status(200).send('Producto creado')
+productRouter.get('/:id?', async (req, res) => {
+  if (req.params.id){
+    console.log("PARAMETROS: ", req.params.id)
+    const data = await productmodel.getOneProduct(Number(req.params.id))
+    console.log(data)
+    return res.status(200).send({data:data})
+  }
+  const data = await productmodel.getAllProducts()
+  return res.status(200).send({data: data})
 })
 
-productRouter.delete('/delete/:id', (req, res) => {
-  return res.status(200).send('Producto eliminado')
+
+productRouter.delete('/delete/:id', async (req, res) => {
+  const data = await productmodel.deleteProduct(req.params.id)
+  return res.status(200).send({data: data})
 })
 
-productRouter.put('/:id', (req, res) => {
+productRouter.put('/:id', async (req, res) => {
+  const data = await productmodel.updateProduct(req.params.id, req.body)
   return res.status(200).send('Producto actualizado')
 })
+
 
 export default productRouter
